623640dc07fedf75890efc62090191ccfb58e6e2
Marcos Maia <maia.marcos@gmail.com>
2019-10-06 00:25:01 +0200

    small adjusments on the prototype version

--- src/main/kotlin/io/resona/springkafkakotlin/KotlinProducer.kt
+++ src/main/kotlin/io/resona/springkafkakotlin/KotlinProducer.kt
@@ -9,12 +9,4 @@ class KotlinProducer(private val kafkaTemplate: KafkaTemplate<String, String>) {
   fun send(message: String) {
     kafkaTemplate.send("simple-message-topic", message)
   }
-
-  fun send(key: String, message: String) {
-    kafkaTemplate.send("simple-message-topic", key, message)
-  }
-
-  fun send(topic: String, key:String, message:String) {
-    kafkaTemplate.send(topic, key, message)
-  }
 }
