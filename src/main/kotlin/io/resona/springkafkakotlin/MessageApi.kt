7219065059d914c6f873ee58c17c79cdd7318ff2
Marcos Maia <maia.marcos@gmail.com>
2019-10-07 22:15:02 +0200

    working version 0.2 matched with post, missing the part about the configuration but will publish as first version online.

--- src/main/kotlin/io/resona/springkafkakotlin/MessageApi.kt
+++ src/main/kotlin/io/resona/springkafkakotlin/MessageApi.kt
@@ -2,11 +2,13 @@ package io.resona.springkafkakotlin
 
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/api")
 class MessageApi(private val kotlinProducer: KotlinProducer) {
-  @PostMapping("publish")
+  @PostMapping("/message")
   fun publish(@RequestBody message: String) {
     kotlinProducer.send(message)
   }
